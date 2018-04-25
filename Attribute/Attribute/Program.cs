87d02050071901ab78d5dda95d7ff2d665908d48
FireHappy <302652339@qq.com>
2018-04-25 16:45:25 +0800

    打印attribute属性

--- Attribute/Attribute/Program.cs
+++ Attribute/Attribute/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +22,24 @@ namespace Attribute
 
         static void GetTypeAttributeInfo<T>()
         {        
-            Console.WriteLine(typeof(T).GetCustomAttributes(typeof(HelpAttribute),false));           
+            //取得自定义特性
+            IEnumerable<System.Attribute> attributes= typeof(T).GetCustomAttributes();
+            var enumerable = attributes as System.Attribute[] ?? attributes.ToArray();
+            //while (enumerable.GetEnumerator().MoveNext())
+            //{
+            //    Console.WriteLine(enumerable.GetEnumerator().MoveNext());  
+            //    Console.WriteLine(enumerable.GetEnumerator().Current);
+            //}
+            //遍历特性
+            foreach (var attribute in enumerable)
+            {
+                Console.WriteLine(attribute);
+                if (attribute.GetType() == typeof(HelpAttribute))
+                {
+                    HelpAttribute help = (HelpAttribute) attribute;
+                    Console.WriteLine("help.Description:" + help.Description);
+                }
+            }
         }
     }
 }
